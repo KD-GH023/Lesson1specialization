17472da6bed8f468f0a9e50ad8f629acc279be3b
Oksana <oksanasolo@gmail.com>
2023-03-12 17:14:45 +0300

    РЕШЕНИЕ

--- Program.cs
+++ Program.cs
@@ -4,4 +4,13 @@ string[] A;
 Console.WriteLine("Введите массив строк/через пробел");
  string stroka = Console.ReadLine();
  A = stroka.Split(' ');
+var result = new string[A.Length];
+ var realSize = 0;
+ foreach (var value in A )
+ {
+ if (value.Length <= 3)
+ {
+  result[realSize] = value;
+    realSize++;
+  }
 
